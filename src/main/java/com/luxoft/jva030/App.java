3f758a39b6d499a14aaf5583dd8141f0b09dc67f
Bartek Kuczynski <BKuczynski@luxoft.com>
2017-06-30 17:46:07 +0200

    Fix compilation errors

--- src/main/java/com/luxoft/jva030/App.java
+++ src/main/java/com/luxoft/jva030/App.java
@@ -1,13 +1,17 @@
 package com.luxoft.jva030;
 
+import io.vavr.control.Either;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+        public static void main(String[] args) {
+            Either.left(new Throwable());
+            Either.right("");
+
+            Either<Throwable, String> e = Either.right("");
+
+
+        }
 }
