465d3b553ddd306997ac3b27a682464e6a6b87a9
Bartek Kuczynski <BKuczynski@luxoft.com>
2017-06-30 08:52:14 +0200

    Init

---
+++ src/main/java/com/luxoft/jva030/App.java
@@ -0,0 +1,13 @@
+package com.luxoft.jva030;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
